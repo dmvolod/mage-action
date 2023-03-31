5f2c6878b3aecdda4256fed3c6f03821c4f62178
Dmitry Volodin <dmvolod@gmail.com>
2023-03-31 22:31:32 +0300

    Test infrastructure (delete)

---
+++ magefile.go
@@ -0,0 +1,21 @@
+//go:build mage
+// +build mage
+
+package main
+
+import (
+	"fmt"
+)
+
+func Mage() error {
+	fmt.Println("Mage")
+
+	return nil
+}
+
+func Lint() error {
+	fmt.Println("Lint")
+
+	return nil
+}
+
